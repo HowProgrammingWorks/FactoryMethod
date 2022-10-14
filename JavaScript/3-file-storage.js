b9cdba88d257d938de4ae753d46a1a1308291cb5
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-10-15 01:43:56 +0300

    Add node: for internal modules

--- JavaScript/3-file-storage.js
+++ JavaScript/3-file-storage.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const fs = require('fs');
-const readline = require('readline');
+const fs = require('node:fs');
+const readline = require('node:readline');
 
 class Database {
   constructor() {
